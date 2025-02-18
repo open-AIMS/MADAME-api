b26fcca5513f66e4c364836c74bc535443c11709
Daniel Tan <d.tan@aims.gov.au>
2025-02-18 11:44:37 +1100

    change invoke run to parse json

--- src/routing/model_run.jl
+++ src/routing/model_run.jl
@@ -23,7 +23,7 @@ Makie.set_theme!(WGLMakie=(framerate=25, ))
         <input type="text" id="scenario-name" name="run"><br>
 
         <label for="ssps">Representative Concentration Pathways:</label><br>
-        <input type="text" id="scenario-name" name="run" value="This does nothing, but could be one or more SSPs/RCPs"><br>
+        <input type="text" id="scenario-name" name="RCP" value="This does nothing, but could be one or more SSPs/RCPs"><br>
 
         <label for="scenario-set-size">Scenario set size:</label><br>
         <input type="text" id="scenario-set-size" name="scenario-runs" value="32"><br><br>
@@ -39,27 +39,13 @@ Makie.set_theme!(WGLMakie=(framerate=25, ))
     """)
 end
 
-# Parse the form data and return it
-@post "/form" function (req)
-    data = formdata(req)
-    return data
-end
+function run_coral_blox(
+    ta_lower,
+    ta_upper,
+    step_size,
+    n_runs
+)::ADRIA.ResultSet
 
-@post "/invoke-run/coralblox" function (req)
-    data = formdata(req)
-
-    # Parse and validate inputs
-    scenario_name = get(data, "run", nothing)
-
-    n_runs = parse(Int, get(data, "scenario-runs", "64"))
-    n_runs <= 0 && error("Scenario set size must be positive and a power of 2")
-
-    ta_lower = parse(Float64, get(data, "seed-TA-lower_bound", "100000"))
-    ta_upper = parse(Float64, get(data, "seed-TA-upper_bound", "1000000"))
-    step_size = floor(Int64, (ta_upper - ta_lower) / 10)
-
-    # TODO: If user provides a list of SSPs/RCPs to run, have to loop and combine results
-    #       at the end.
     default_domain_dir = Base.get_preferences()["default_domain"]
     dom = ADRIA.load_domain(default_domain_dir, "45")
 
@@ -68,9 +54,47 @@ end
     scens = ADRIA.sample(dom, n_runs)
     rs = ADRIA.run_scenarios(dom, scens, "45")
 
-    r_tac = ADRIA.metrics.scenario_total_cover(rs)
-    # f = ADRIA.viz.scenarios(rs, r_tac)
+    return rs
+end
 
+function default_save_name(rs::ADRIA.ResultSet)::String
+    return "$(rs.name)__RCPs_$(rs.RCP)__$(rs.invoke_time)"
+end
+
+# Parse the form data and return it
+@post "/form" function (req)
+    data = formdata(req)
+    return data
+end
+
+@post "/invoke-run/coralblox" function (req)
+    println(req)
+    data = json(req)["params"]
+
+    # Parse and validate inputs
+    scenario_name = get(data, "runName", nothing)
+
+    res_dir = Base.get_preferences()["resultsets_dir"]
+    new_path = joinpath(res_dir, scenario_name)
+
+    if isdir(new_path)
+        return HTTP.Response(400, "Model Run name already used.")
+    end
+
+    n_runs = parse(Int, get(data, "numScenarios", "64"))
+    n_runs <= 0 && error("Scenario set size must be positive and a power of 2")
+
+    ta_lower = parse(Float64, get(data, "ta_lower", "100000"))
+    ta_upper = parse(Float64, get(data, "ta_upper", "1000000"))
+    step_size = floor(Int64, (ta_upper - ta_lower) / 10)
+
+    # TODO: If user provides a list of SSPs/RCPs to run, have to loop and combine results
+    #       at the end.
+    rs = run_coral_blox(ta_lower, ta_upper, step_size, n_runs)
+
+    default_path = joinpath(res_dir, default_save_name(rs))
+    
+    mv(default_path, new_path)
     # return Page(export_fig(f))
-    return html(ADRIA.viz.scenarios(rs, r_tac))
+    return json(:run_name => scenario_name)
 end
