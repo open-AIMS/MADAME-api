37ca77baee5cd04492a7c9828010a826a069a14a
Daniel Tan <d.tan@aims.gov.au>
2025-02-19 10:05:20 +1100

    document model run invocation

--- src/routing/model_run.jl
+++ src/routing/model_run.jl
@@ -86,30 +86,6 @@ function default_save_name(rs::ADRIA.ResultSet)::String
     return "$(rs.name)__RCPs_$(rs.RCP)__$(rs.invoke_time)"
 end
 
-"""
-    @post "/invoke-run/coralblox" function (req)
-
-Endpoint to allow the end user to invoke model runs. The request body should be json of the 
-following form.
-
-# Example
-```json
-{
-  "run_name": <run_name>,
-  "num_scenarios": <num_scenarios>,
-  "model_params": [
-    {
-      "param_name": name,
-      "third_param_flag": <Boolean>,
-      "lower": <lower>,
-      "upper": <upper>,
-      "optional_third": 
-    },
-    ...
-  ]
-}
-```
-"""
 @post "/invoke-run/coralblox" function (req)
     println(req)
     response = json(req, InvokeRunResponse)
