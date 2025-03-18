1781e2e0a97485764beb32d158a94ae7dba97d2d
Daniel Tan <d.tan@aims.gov.au>
2025-03-19 10:24:12 +1100

    remove broken configuration check
    add oxygen and http dependencies to main setup

--- src/MADAMEAPI.jl
+++ src/MADAMEAPI.jl
@@ -1,6 +1,10 @@
 module MADAMEAPI
 
+using Oxygen,
+    HTTP
+
 using ADRIA
+
 using TOML
 
 include("util.jl")
@@ -13,21 +17,7 @@ include("run/invoke_run.jl")
 function start_server()
     @info "Launching Server... please wait"
 
-    ADRIA_config_path::String = joinpath(pwd(), "config.toml")
-    if isfile(ADRIA_config_path)
-        ADRIA_outdir::String = TOML.parsefile(ADRIA_config_path)["results"]["output_dir"]
-        if !samefile(ADRIA_outdir, Base.get_preferences()["resultsets_dir"])
-            msg = "ADRIA model run output directory is not the same as the server result "
-            msg *= "store directory. New model runs will not appear in the server results."
-            @warn msg
-        end
-    else
-        if !samefile(joinpath(pwd(), "Outputs"), Base.get_preferences()["resultsets_dir"])
-            msg = "ADRIA model run output directory is not the same as the server result "
-            msg *= "store directory. New model runs will not appear in the server results."
-            @warn msg
-        end
-    end
+    # TO DO: check ADRIA output path aligns with local preferences path
 
     @info "Setting up result metadata routes..."
     setup_result_metadata_routes()
